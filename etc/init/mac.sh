b99fdae6368c3781f794f1887d3aa68ba080ac13
AtsushiSaito <atsushi31100854@gmail.com>
2018-11-12 16:17:48 +0900

    Add mac.sh
    - macOS用の初期セットアップスクリプトを追加

---
+++ etc/init/mac.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config --global user.name "AtsushiSaito"
+git config --global user.email "atsushi31100854@gmail.com"
+git config --global core.editor 'vim -c "set fenc=utf-8"'
+
+brew install lua
+brew install vim --with-lua
