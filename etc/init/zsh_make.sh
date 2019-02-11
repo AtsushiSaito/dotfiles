74f37d7b87e184807eb6e5fb44f2440011f30121
AtsushiSaito <atsushi31100854@gmail.com>
2019-02-12 04:02:45 +0900

    Update zsh_make.sh

--- etc/init/zsh_make.sh
+++ etc/init/zsh_make.sh
@@ -3,9 +3,12 @@
 sudo apt update
 sudo apt-get install libncursesw5-dev
 cd /usr/local/src
-sudo wget https://sourceforge.net/projects/zsh/files/zsh/5.5.1/zsh-5.5.1.tar.xz/download -O zsh-5.5.1.tar.xz
-sudo tar xvf zsh-5.5.1.tar.xz
-cd zsh-5.5.1
+#sudo wget https://sourceforge.net/projects/zsh/files/zsh/5.5.1/zsh-5.5.1.tar.xz/download -O zsh-5.5.1.tar.xz
+sudo wget https://sourceforge.net/projects/zsh/files/zsh/5.7.1/zsh-5.7.1.tar.xz/download -O zsh-5.7.1.tar.xz
+#sudo tar xvf zsh-5.5.1.tar.xz
+#cd zsh-5.5.1
+sudo tar xvf zsh-5.7.1.tar.xz
+cd zsh-5.7.1
 sudo ./configure --enable-multibyte
 sudo make
 sudo make install
