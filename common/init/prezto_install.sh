6c1e69ab31d04e6fc69cf3e9b03e4ce9a857ebbb
AtsushiSaito <atsushi31100854@gmail.com>
2019-12-17 02:20:26 +0900

    Update

--- common/init/prezto_install.sh
+++ common/init/prezto_install.sh
@@ -6,7 +6,7 @@ else
 	git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 	setopt EXTENDED_GLOB
 	for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-		ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+		ln -fs "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 	done
 fi
 
